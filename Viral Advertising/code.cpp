25bcfc4cb5a50efe58a77c05c9782b45a590e89c
vinay kumar <61869982+zerodays39@users.noreply.github.com>
2021-01-27 18:42:38 +0530

    Update code.cpp

--- Viral Advertising/code.cpp
+++ Viral Advertising/code.cpp
@@ -1,1 +1,42 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//user code
+
+int viralAdvertising(int n)
+ {
+     int p=5,l=0,tl=0;
+     
+     for(int i=0;i<n;i++)
+     {
+         l= p/2;
+        tl=tl+l;
+         p= l*3;
+         
+     }
+     return tl;
+}
+
+
+//user code
+
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = viralAdvertising(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
